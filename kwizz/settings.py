c3dd9cf18b07381ecdfd60d2845fd70903d69f8e
theaman05 <theaman1805@gmail.com>
2023-12-08 11:36:16 +0530

    added database connection string

--- kwizz/settings.py
+++ kwizz/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +85,11 @@ WSGI_APPLICATION = 'kwizz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# for postgres with database url
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
+
 # for postgres
 # DATABASES = {
 #     'default': {
@@ -97,12 +103,12 @@ WSGI_APPLICATION = 'kwizz.wsgi.application'
 # }
 
 # For sqlite
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
